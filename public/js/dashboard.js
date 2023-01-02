35c65f006f3196d3ea514f8de0301701ce76ca3f
lilianaba <52921112+lilianaba@users.noreply.github.com>
2023-01-02 11:28:37 +1030

    comments and post working

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,6 +4,7 @@ const newFormHandler = async (event) => {
     const title = document.querySelector('#post-title').value.trim();
     const content = document.querySelector('#post-content').value.trim();
   
+    console.log(title,content)
     if (title && content) {
       const response = await fetch(`/api/post`, {
         method: 'POST',
